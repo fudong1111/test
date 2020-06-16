b0add21333bf7d39e73fc56bb5ab0e9ec3e0c4a9
fudong1111 <51374041+fudong1111@users.noreply.github.com>
2020-06-16 23:39:09 +0800

    Create pythoncopyfile.py

---
+++ pythoncopyfile.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+
+A = "Local_Script"
+B = "Local_back"
+#通过校验MD5 判断B内的文件与A 不同
+def get_MD5(file_path):
+    files_md5 = os.popen('md5 %s' % file_path).read().strip()
+    file_md5 = files_md5.replace('MD5 (%s) = ' % file_path, '')
+    return file_md5
+
+
+
+def main(path, out):
+    for files in os.listdir(path):
+        name = os.path.join(path, files)
+        back_name = os.path.join(out, files)
+        if os.path.isfile(name):
+            if os.path.isfile(back_name):
+                if get_MD5(name) != get_MD5(back_name):
+                    shutil.copy(name,back_name)     
+            else:
+                shutil.copy(name, back_name)
+        else:
+            if not os.path.isdir(back_name):
+                os.makedirs(back_name)
+            main(name, back_name)
+
+
+if __name__ == '__main__':
+    main(A, B)
+
+#拷贝目录A 的内容到目录B
